c541afbcd63b31614d30ea4235108d1376126ae2
cernochmartin <cernochm@outlook.cz>
2025-02-01 14:41:46 +0100

    feat: update versions of packages

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,14 +1,18 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['../src/module'],
+
   msal: {
     clientId: '',
     redirectUri: ''
   },
+
   runtimeConfig: {
     public: {
       CLIENT_ID: process.env.CLIENT_ID,
       REDIRECT_URI: process.env.REDIRECT_URI
     }
-  }
+  },
+
+  compatibilityDate: '2025-02-01'
 })
